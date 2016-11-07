eb103609d0057eb31073ba6cc7cef5980223ff5c
Joel Ross <joelross@uw.edu>
2016-11-07 10:24:07 -0800

    End of lecture code (to be continued)

--- src/MovieController.js
+++ src/MovieController.js
@@ -1,4 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
 //TMDB constants (https://developers.themoviedb.org/3/getting-started)
 var apiKey = "d37398a8fa01ed9f121f9074b614e320";
 var baseApiUrl = "https://api.themoviedb.org/3";
 var baseImageUrl = "https://image.tmdb.org/t/p/w92"; //small posters
+
+var controller = {
+
+  //download data from the url
+  search: function(searchQuery) {    
+    //construct URL
+    var resource = '/search/movie';
+    var uri = baseApiUrl + resource + '?query='+searchQuery +'&api_key='+apiKey;
+    console.log('fetching from', uri);
+
+    fetch(uri) //download the data
+      .then(function(res) { return res.json(); })
+      .then(function(data) {
+        //re-render with new data (pass in as a prop)
+        ReactDOM.render(<App data={data} />,
+         document.querySelector('#root'));
+      });
+  },
+
+  getPosterUrl: function(movie){
+    if(movie.poster_path) {
+      return baseImageUrl + movie.poster_path;
+    }
+    else {
+      return ''; //don't load bad image'
+    }
+  }
+  //other functions can go here
+};
+
+export default controller; //export object
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import MovieController from './MovieController';
 
 //css files
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+MovieController.search("titanic");
+
+// ReactDOM.render(
+//   <App />,
+//   document.querySelector('#root')
+// );
